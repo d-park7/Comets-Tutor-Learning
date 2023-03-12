58cfc98a42e83b973f5d6ddd9b8243eeb3f18269
d-park7 <davidpark450@gmail.com>
2023-03-11 21:23:40 -0600

    login implemented

--- api/api.py
+++ api/api.py
@@ -169,3 +169,37 @@ async def delete_appointment(id: str):
         return Response(status_code=status.HTTP_204_NO_CONTENT)
 
     raise HTTPException(status_code=404, detail=f"Appointment {id} not found")
+
+
+
+# # User Auth
+
+
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+async def get_user(db, email: str):
+    if (student := await db["students"].find_one({"email": email})) is not None:
+        return student
+
+
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = await get_user(db, form_data.username)
+    print(user)
+    if not user:
+        raise HTTPException(status_code=404, detail=f"Incorrect username or password")
+
+    password = form_data.password
+    if not password == user["password"]:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user["name"], "token_type": "bearer"}
+
+@app.get("/users/me")
+async def read_users_me(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
