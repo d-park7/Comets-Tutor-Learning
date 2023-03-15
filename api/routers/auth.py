076aa08bb16d07434b9c4be22c300e303fc67287
d-park7 <davidpark450@gmail.com>
2023-03-14 21:02:44 -0500

    Fleshed out login auth

--- api/routers/auth.py
+++ api/routers/auth.py
@@ -1,24 +1,23 @@
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException, APIRouter
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from api.database import db
-from api.api import app
 
 
-# the tokenurl is the url path for the authorization used in read_users_me
-# the read_users_me() returns the token to be used in the login()
+router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 async def get_user(db, email: str):
     if (student := await db["students"].find_one({"email": email})) is not None:
         return student
+    elif (tutor := await db["tutors"].find_one({"email": email})) is not None:
+        return tutor
 
 
-@app.post("/token")
+@router.post("/token")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     user = await get_user(db, form_data.username)
-    print(user)
     if not user:
         raise HTTPException(status_code=404, detail=f"Incorrect username or password")
 
@@ -29,6 +28,6 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     return {"access_token": user["name"], "token_type": "bearer"}
 
 
-@app.get("/users/me")
+@router.get("/users/me")
 async def read_users_me(token: str = Depends(oauth2_scheme)):
-    return {"token": token} 
+    return {"token": token}
---
+++ api/routers/students.py
@@ -0,0 +1,62 @@
+from fastapi import APIRouter, Body, status, HTTPException
+from fastapi.responses import JSONResponse, Response
+from fastapi.encoders import jsonable_encoder
+from typing import List
+
+from api.database import db
+from api.schemas import Student, UpdateStudentModel
+
+
+
+router = APIRouter()
+
+
+@router.post("/createstudent", response_description="Add new student", response_model=Student)
+async def create_student(student: Student = Body(...)):
+    student = jsonable_encoder(student)
+    new_student = await db["students"].insert_one(student)
+    created_student = await db["students"].find_one({"_id": new_student.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_student)
+
+
+@router.get("/students", response_description="List all students", response_model=List[Student])
+async def list_students():
+    students = await db["students"].find().to_list(1000)
+    return students
+
+
+@router.get("/student/{id}", response_description="Get a single student", response_model=Student)
+async def show_student(id: str):
+    if (student := await db["students"].find_one({"_id": id})) is not None:
+        return student
+
+    raise HTTPException(status_code=404, detail=f"Student {id} not found line 39")
+
+
+@router.put("/updatestudent/{id}", response_description="Update a student", response_model=UpdateStudentModel)
+async def update_student(id: str, student: UpdateStudentModel = Body(...)):
+    student = {k: v for k, v in student.dict().items() if v is not None}
+
+    if len(student) >= 1:
+        update_result = await db["students"].update_one({"_id": id}, {"$set": student})
+
+        if update_result.modified_count == 1:
+            if (
+                updated_student := await db["students"].find_one({"_id": id})
+            ) is not None:
+                return updated_student
+
+    if (existing_student := await db["students"].find_one({"_id": id})) is not None:
+        return existing_student
+
+    raise HTTPException(status_code=404, detail=f"Student {id} not found")
+
+
+@router.delete("/student/{id}", response_description="Delete a student")
+async def delete_student(id: str):
+    delete_result = await db["students"].delete_one({"_id": id})
+
+    if delete_result.deleted_count == 1:
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    raise HTTPException(status_code=404, detail=f"Student {id} not found")
